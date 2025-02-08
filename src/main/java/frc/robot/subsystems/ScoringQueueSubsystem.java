24bf0e95c4823994256966bbb2dcb968d1057c6d
Tilghman97 <tilghmanwescottreiss@gmail.com>
2025-02-08 11:29:22 -0500

    Added Elastic

--- src/main/java/frc/robot/subsystems/ScoringQueueSubsystem.java
+++ src/main/java/frc/robot/subsystems/ScoringQueueSubsystem.java
@@ -25,10 +25,39 @@ public class ScoringQueueSubsystem extends SubsystemBase {
     L4
   }
 
+  private boolean QueuedL1 = false;
+  private boolean QueuedLeftL2 = false;
+  private boolean QueuedLeftL3 = false;
+  private boolean QueuedLeftL4 = false;
+  private boolean QueuedRightL2 = false;
+  private boolean QueuedRightL3 = false;
+  private boolean QueuedRightL4 = false;
+
   public void addScoringCommand(ScoringSide side, ScoringHeight height) {
 
     // Systsem log for scoring side and height
     System.out.println("Scoring command added to queue: " + side + " " + height);
+
+    if (side == ScoringSide.LEFT) {
+      if (height == ScoringHeight.L2) {
+        QueuedLeftL2 = true;
+      } else if (height == ScoringHeight.L3) {
+        QueuedLeftL3 = true;
+      } else if (height == ScoringHeight.L4) {
+        QueuedLeftL4 = true;
+      }
+    }
+    if (height == ScoringHeight.L1) {
+      QueuedL1 = true;
+    } else {
+      if (height == ScoringHeight.L2) {
+        QueuedRightL2 = true;
+      } else if (height == ScoringHeight.L3) {
+        QueuedRightL3 = true;
+      } else if (height == ScoringHeight.L4) {
+        QueuedRightL4 = true;
+      }
+    }
 
     // // Clear the queue before adding a new command
     // commandQueue.clear();
@@ -44,6 +73,14 @@ public class ScoringQueueSubsystem extends SubsystemBase {
 
   public void clearQueue() {
     commandQueue.clear();
+    QueuedL1 = false;
+    QueuedLeftL2 = false;
+    QueuedLeftL3 = false;
+    QueuedLeftL4 = false;
+
+    QueuedRightL2 = false;
+    QueuedRightL3 = false;
+    QueuedRightL4 = false;
   }
 
   @Override
@@ -55,5 +92,14 @@ public class ScoringQueueSubsystem extends SubsystemBase {
             .toArray(String[]::new);
 
     SmartDashboard.putStringArray("Scoring Queue", queueEntries);
+
+    SmartDashboard.putBoolean("QueuedL1", QueuedL1);
+    SmartDashboard.putBoolean("QueuedLeftL2", QueuedLeftL2);
+    SmartDashboard.putBoolean("QueuedLeftL3", QueuedLeftL3);
+    SmartDashboard.putBoolean("QueuedLeftL4", QueuedLeftL4);
+
+    SmartDashboard.putBoolean("QueuedRightL2", QueuedRightL2);
+    SmartDashboard.putBoolean("QueuedRightL3", QueuedRightL3);
+    SmartDashboard.putBoolean("QueuedRightL4", QueuedRightL4);
   }
 }
