bce270a6ee7237c967cc0a425c1ef4b85d587ceb
Pekay <github@maxlingenfelter.com>
2025-02-11 18:16:30 -0500

    Update Music.java

--- src/main/java/frc/robot/subsystems/Music.java
+++ src/main/java/frc/robot/subsystems/Music.java
@@ -1,29 +1,30 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-//Tilghman97WasHere
-//DONOTTESTWITHOUTMEEEEEEEEEEEEEEEEE pls
+// Tilghman97WasHere
+// DONOTTESTWITHOUTMEEEEEEEEEEEEEEEEE pls
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.Orchestra;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 public class Music extends SubsystemBase {
 
-//Define Motors
-private final TalonFX FrontLeftDrive = new TalonFX(10);
-private final TalonFX FrontLeftTurn = new TalonFX(15);
+  // Define Motors
+  private final TalonFX FrontLeftDrive = new TalonFX(10);
+  private final TalonFX FrontLeftTurn = new TalonFX(15);
 
-private final TalonFX FrontRightDrive = new TalonFX(20);
-private final TalonFX FrontRightTurn = new TalonFX(25);
+  private final TalonFX FrontRightDrive = new TalonFX(20);
+  private final TalonFX FrontRightTurn = new TalonFX(25);
 
-private final TalonFX BackLeftDrive = new TalonFX(30);
-private final TalonFX BackLeftTurn = new TalonFX(35);
+  private final TalonFX BackLeftDrive = new TalonFX(30);
+  private final TalonFX BackLeftTurn = new TalonFX(35);
 
-private final TalonFX BackRightDrive = new TalonFX(40);
-private final TalonFX BackRightTurn = new TalonFX(45);
+  private final TalonFX BackRightDrive = new TalonFX(40);
+  private final TalonFX BackRightTurn = new TalonFX(45);
 
   private Orchestra m_orchestra = new Orchestra();
   /** Creates a new Music. */
@@ -33,21 +34,21 @@ private final TalonFX BackRightTurn = new TalonFX(45);
     m_orchestra.addInstrument(FrontRightDrive, 0);
     m_orchestra.addInstrument(FrontRightTurn, 0);
 
-    //add back motors to different track to add ability for chords
-    m_orchestra.addInstrument(BackLeftDrive, 1);
-    m_orchestra.addInstrument(BackLeftTurn, 1);
-    m_orchestra.addInstrument(BackRightDrive, 1);
-    m_orchestra.addInstrument(BackRightTurn, 1);
+    // add back motors to different track to add ability for chords
+    m_orchestra.addInstrument(BackLeftDrive, 0);
+    m_orchestra.addInstrument(BackLeftTurn, 0);
+    m_orchestra.addInstrument(BackRightDrive,0);
+    m_orchestra.addInstrument(BackRightTurn, 0);
   }
 
   public void playUnderTheSea() {
     // stop any music that is currently playing
     m_orchestra.stop();
 
-    //load Under The Sea
+    // load Under The Sea
     m_orchestra.loadMusic("UnderTheSea.chrp");
 
-    //play Under The Sea
+    // play Under The Sea
     m_orchestra.play();
   }
 
@@ -55,10 +56,10 @@ private final TalonFX BackRightTurn = new TalonFX(45);
     // stop any music that is currently playing
     m_orchestra.stop();
 
-    //load Final Countdown
+    // load Final Countdown
     m_orchestra.loadMusic("TheFinalCountdown.chrp");
 
-    //play Final Countdown
+    // play Final Countdown
     m_orchestra.play();
   }
 
@@ -66,10 +67,10 @@ private final TalonFX BackRightTurn = new TalonFX(45);
     // stop any music that is currently playing
     m_orchestra.stop();
 
-    //load We Are The Champions
+    // load We Are The Champions
     m_orchestra.loadMusic("WeAreTheChampions.chrp");
 
-    //play We Are The Champions
+    // play We Are The Champions
     m_orchestra.play();
   }
 
