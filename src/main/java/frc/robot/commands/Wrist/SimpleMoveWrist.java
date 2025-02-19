ffe08ec07aeb090f0b1dc8f161ae3e6d80e3b2ba
Max Lingenfelter <github@maxlingenfelter.com>
2025-02-19 16:40:22 -0500

    Update SimpleMoveWrist.java

--- src/main/java/frc/robot/commands/Wrist/SimpleMoveWrist.java
+++ src/main/java/frc/robot/commands/Wrist/SimpleMoveWrist.java
@@ -38,6 +38,12 @@ public class SimpleMoveWrist extends Command {
     double speed = speedSupplier.getAsDouble();
     speed = speed * Wrist.SPEED_SCALAR;
     Logger.recordOutput("SimpleMoveWrist/execute", "Wrist Speed: " + speed);
+    Logger.recordOutput("SimpleMoveWrist/angle", "Wrist Angle: " + m_wrist.getAngle());
+
+
+    Logger.recordOutput("Wrist/GivenSpeed", speed);
+
+
     m_wrist.moveManual(speed);
   }
 
