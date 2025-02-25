ed7c2a6f3769e7d1e6554b448134e87be1c9bc64
Max Lingenfelter <github@maxlingenfelter.com>
2025-02-25 16:20:32 -0500

    Trying to fix and symflify things

--- src/main/java/frc/robot/commands/Wrist/AdaptiveWrist.java
+++ src/main/java/frc/robot/commands/Wrist/AdaptiveWrist.java
@@ -4,51 +4,107 @@
 
 package frc.robot.commands.Wrist;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.Wrist;
 import frc.robot.subsystems.IntakeSubsystem;
-import java.util.function.DoubleSupplier;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class AdaptiveWrist extends Command {
   private final IntakeSubsystem m_intake;
   private final DoubleSupplier wristAngleSupplier;
   private final boolean isPickup;
-  private final RunCoralIntake coralIntake;
-  private double speed;
-
+  
+  // The separate coral intake command
+  private RunCoralIntake coralIntakeCommand;
+  private boolean isRunningCoralIntake = false;
+  private boolean wasInCoralRange = false;
+  
   public AdaptiveWrist(
       IntakeSubsystem intake, DoubleSupplier wristAngleSupplier, boolean isPickup) {
     this.m_intake = intake;
     this.wristAngleSupplier = wristAngleSupplier;
     this.isPickup = isPickup;
-
-    coralIntake = new RunCoralIntake();
-
+    
     addRequirements(m_intake);
+  }
+  
+  @Override
+  public void initialize() {
+    // Create a new instance of the coral intake command
+    coralIntakeCommand = new RunCoralIntake(m_intake, true);
+    isRunningCoralIntake = false;
+    wasInCoralRange = false;
   }
 
   @Override
   public void execute() {
     double wristAngle = wristAngleSupplier.getAsDouble();
     boolean isCoralRange = wristAngle < Wrist.CORAL_MAX_ANGLE;
+    
+    // Check for mode changes
+    if (isCoralRange != wasInCoralRange) {
+      wasInCoralRange = isCoralRange;
+      
+      // If we're leaving coral range and were running coral intake, end it
+      if (!isCoralRange && isRunningCoralIntake) {
+        coralIntakeCommand.end(true);
+        isRunningCoralIntake = false;
+      }
+    }
 
-    runIntake(isCoralRange);
-  }
-
-  private void runIntake(boolean isCoralRange) {
+    // Run appropriate intake/outtake based on mode
     if (isCoralRange) {
       if (isPickup) {
-        coralIntake.runIntake();
+        // Handle coral pickup using the separate command
+        handleCoralIntake();
       } else {
+        // If switching from intake to outtake, end the coral intake command
+        if (isRunningCoralIntake) {
+          coralIntakeCommand.end(true);
+          isRunningCoralIntake = false;
+        }
+        // Outtake coral
         m_intake.moveRoller(Wrist.Roller.CORAL_OUTTAKE_SPEED);
       }
     } else {
+      // We're in algae range
       if (isPickup) {
         m_intake.moveRoller(Wrist.Roller.ALGAE_INTAKE_SPEED);
       } else {
         m_intake.moveRoller(Wrist.Roller.ALGAE_OUTTAKE_SPEED);
       }
+    }
+  }
+  
+  /**
+   * Manages the coral intake command lifecycle
+   */
+  private void handleCoralIntake() {
+    // Start the coral intake command if not already running
+    if (!isRunningCoralIntake) {
+      coralIntakeCommand.initialize();
+      isRunningCoralIntake = true;
+    }
+    
+    // Run a single iteration of the coral intake command
+    coralIntakeCommand.execute();
+    
+    // Check if coral intake is finished
+    if (coralIntakeCommand.isFinished()) {
+      // Successfully completed coral intake
+      coralIntakeCommand.end(false);
+      // Keep isRunningCoralIntake as true to prevent restarting the intake process
+      // until we switch modes
+    }
+  }
+  
+  @Override
+  public void end(boolean interrupted) {
+    // Make sure to properly end the coral intake command if it's running
+    if (isRunningCoralIntake) {
+      coralIntakeCommand.end(interrupted);
+      isRunningCoralIntake = false;
     }
   }
 }
--- src/main/java/frc/robot/commands/Wrist/RunCoralIntake.java
+++ src/main/java/frc/robot/commands/Wrist/RunCoralIntake.java
@@ -1,50 +1,76 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands.Wrist;
-
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.Wrist;
 import frc.robot.subsystems.IntakeSubsystem;
-
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class RunCoralIntake extends Command {
   /** Creates a new RunIntake. */
-  private IntakeSubsystem intakeSubsystem;
-
+  private final IntakeSubsystem intakeSubsystem;
+  private final boolean autoStop;
+  private boolean coralCentered = false;
+  private final Timer timer;
+  private boolean firstSensorActivated = false;
+  
+  public RunCoralIntake(IntakeSubsystem intakeSubsystem, boolean autoStop) {
+    this.intakeSubsystem = intakeSubsystem;
+    this.autoStop = autoStop;
+    timer = new Timer();
+    addRequirements(intakeSubsystem);
+  }
+  
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    intakeSubsystem = new IntakeSubsystem();
+    coralCentered = false;
+    firstSensorActivated = false;
+    timer.stop();
+    timer.reset();
   }
-
+  
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
-
-  public void runIntake() {
-    intakeSubsystem.moveRoller(Wrist.Roller.CORAL_INTAKE_SPEED);
-    if (intakeSubsystem.getFirstSensor()) {
-      intakeSubsystem.moveRoller(Wrist.Roller.AFTER_FIRST_SENSOR_CORAL_SPEED);
+  public void execute() {
+    // If first sensor hasn't been activated yet, run at intake speed
+    if (!firstSensorActivated) {
+      intakeSubsystem.moveRoller(Wrist.Roller.CORAL_INTAKE_SPEED);
+      
+      // Check if first sensor is now activated
+      if (intakeSubsystem.getFirstSensor()) {
+        firstSensorActivated = true;
+        intakeSubsystem.moveRoller(Wrist.Roller.AFTER_FIRST_SENSOR_CORAL_SPEED);
+        timer.reset();
+        timer.start();
+      }
+    } 
+    // If first sensor has been activated, start timing
+    else {
+      // If 0.5 seconds have passed, stop the motor
+      if (timer.get() >= 0.5) {
+        intakeSubsystem.stopRoller();
+        coralCentered = true;
+      }
     }
-    if (intakeSubsystem.getSecondSensor()) {
-      new WaitCommand(1);
-      intakeSubsystem.stopRoller();
-    }
-
-    // If first sensor no longer sees the coral,  roll back until it does
-
   }
-
+  
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
-
+  public void end(boolean interrupted) {
+    timer.stop();
+    if (interrupted) {
+      intakeSubsystem.stopRoller();
+    }
+  }
+  
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    if (autoStop && coralCentered) {
+      return true;
+    }
     return false;
   }
 }
