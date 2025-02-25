9bd9dfa79f8769f740e610e2fdecbb3e5cd8454c
bodiereider <bodiereider@me.com>
2025-02-24 20:07:19 -0500

    changed sensor form DIO to sparkflex

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -7,6 +7,7 @@ import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.ctre.phoenix6.configs.DigitalInputsConfigs;
 import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkFlexConfig;
@@ -19,18 +20,23 @@ import frc.robot.Constants.Wrist;
 public class IntakeSubsystem extends SubsystemBase {
   private final SparkFlex rollerMotor;
   //sensors
-  private final DigitalInput firstSensor;
-  private final DigitalInput secondSensor;
+  private final SparkLimitSwitch firstSensor;
+  private final SparkLimitSwitch secondSensor;
+  
   
 
   public IntakeSubsystem() {
-    // Intake config
 
-    firstSensor = new DigitalInput(Wrist.Roller.FIRST_SENSOR_ID);
-    secondSensor = new DigitalInput(Wrist.Roller.SECOND_SENSOR_ID);
-    
-    
     rollerMotor = new SparkFlex(Wrist.Roller.MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
+
+    firstSensor =
+      rollerMotor.getForwardLimitSwitch();
+    secondSensor = 
+      rollerMotor.getForwardLimitSwitch();
+    
+    
+    
+    
     SparkFlexConfig idleConfig = new SparkFlexConfig();
     idleConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(60);
     rollerMotor.configure(
@@ -48,11 +54,11 @@ public class IntakeSubsystem extends SubsystemBase {
   }
 
   public boolean getFirstSensor() {
-    return firstSensor.get();
+    return firstSensor.isPressed();
   }
 
   public boolean getSecondSensor() {
-    return secondSensor.get();
+    return secondSensor.isPressed();
   }
 
 }
