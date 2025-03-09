b57e7eb624fcf8b33f944366f4f682393ec2b844
Max Lingenfelter <github@maxlingenfelter.com>
2025-03-09 19:09:23 +0000

    Tuning vision deviations a bit

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -26,24 +26,25 @@ public class VisionConstants {
   public static String camera1Name = "limelight-back";
 
   // Basic filtering thresholds
-  public static double maxAmbiguity = 0.3;
+  public static double maxAmbiguity = 0.3; //Increase if rejectiing to many
   public static double maxZError = 0.75;
 
   // Standard deviation baselines, for 1 meter distance and 1 tag
   // (Adjusted automatically based on distance and # of tags)
-  public static double linearStdDevBaseline = 0.02; // Meters
-  public static double angularStdDevBaseline = 0.06; // Radians
+  public static double linearStdDevBaseline = 0.01; // Meters
+  public static double angularStdDevBaseline = 0.02; // Radians
 
   // Standard deviation multipliers for each camera
   // (Adjust to trust some cameras more than others)
-  public static double[] cameraStdDevFactors =
+  // (lower standard deviation = higher trust)
+  public static double[] cameraStdDevFactors = 
       new double[] {
-        1.0, // Camera 0
-        0.5 // Camera 1
+        0.5, // Camera 0
+        0.8 // Camera 1
       };
 
   // Multipliers to apply for MegaTag 2 observations
-  public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
+  public static double linearStdDevMegatag2Factor = 0.2; // More stable than full 3D solve
   public static double angularStdDevMegatag2Factor =
       Double.POSITIVE_INFINITY; // No rotation data available
 }
