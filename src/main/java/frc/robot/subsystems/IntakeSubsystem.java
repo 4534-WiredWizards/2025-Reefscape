60f3fb71193b8bd96f4bce5d2acaaa6b40a2c292
maxlingenfelter <github@maxlingenfelter.com>
2025-03-15 13:24:57 -0400

    Switch limit switchs

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -22,8 +22,8 @@ public class IntakeSubsystem extends SubsystemBase {
 
     rollerMotor = new SparkFlex(Wrist.Roller.MOTOR_ID, SparkLowLevel.MotorType.kBrushless);
 
-    firstSensor = rollerMotor.getForwardLimitSwitch();
-    secondSensor = rollerMotor.getReverseLimitSwitch();
+    firstSensor = rollerMotor.getReverseLimitSwitch();
+    secondSensor = rollerMotor.getForwardLimitSwitch();
 
     SparkFlexConfig idleConfig = new SparkFlexConfig();
     idleConfig.limitSwitch.forwardLimitSwitchEnabled(false);
